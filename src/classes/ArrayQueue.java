260967a901d75c4017a19fbc777ed7d0ca287eaf
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2020-05-22 11:48:34 +0100

    Created VetQueue.java and ArrayQueue.java but method still not implemented.

---
+++ src/classes/ArrayQueue.java
@@ -0,0 +1,17 @@
+package classes;
+
+public interface ArrayQueue {
+
+    public boolean enqueue(String element);
+
+    public String dequeue();
+
+    public String first();
+
+    public String last();
+
+    public int size();
+
+    public boolean isEmpty();
+
+}
---
+++ src/classes/VetQueue.java
@@ -0,0 +1,38 @@
+package classes;
+
+public class VetQueue implements ArrayQueue{
+    @Override
+    public boolean enqueue(String element) {
+        return false;
+    }
+
+    @Override
+    public String dequeue() {
+        return null;
+    }
+
+    @Override
+    public String first() {
+        return null;
+    }
+
+    @Override
+    public String last() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        return null;
+    }
+}
